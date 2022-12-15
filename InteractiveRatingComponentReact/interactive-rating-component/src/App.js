8b2695dd42425386dfd2d6e86946737a1b5551b1
jschneeweiss <j.schneeweiss@aol.com>
2022-12-15 15:07:07 +0100

    feat: create app

---
+++ InteractiveRatingComponentReact/interactive-rating-component/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      How did we do?
+
+      Please let us know how we did with your support request. All feedback is appreciated
+      to help us improve our offering!
+
+      1 2 3 4 5
+
+      Submit
+
+      You selected ** out of 5
+
+      Thank you!
+
+      We appreciate you taking the time to give a rating. If you ever need more support,
+      don’t hesitate to get in touch!
+    </div>
+  );
+}
+
+export default App;
