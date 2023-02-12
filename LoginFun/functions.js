edec921b39ecd4f764f58fdd5a0ae5eefac6a6ff
jschneeweiss <j.schneeweiss@aol.com>
2023-02-12 17:51:49 +0100

    feat: show passwort criteria

--- LoginFun/functions.js
+++ LoginFun/functions.js
@@ -10,11 +10,11 @@ function checkPassword(isClicked) {
     let modal = document.getElementById("modal");
 
     let passw = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
+    
     if (password.match(passw)) {
         if (isClicked) {
             clearInputs();
             modal.style.display = "block";
-            // alert("You created an account :)");
         }
 
         resetButtonPosition();
@@ -35,25 +35,49 @@ function showButton() {
     let password = document.getElementById("psw").value;
 
     if ((password !== "") && checkEmail()) {
-        document.getElementsByClassName("container")[0].style = "height: 14.5rem";
+        document.getElementsByClassName("container")[0].style = "height: 20rem";
         document.getElementsByClassName("login__button")[0].style = "visibility: visible";
+        document.getElementsByClassName("login__passwordchecklist")[0].style = "visibility: visible";
+        document.documentElement.style.setProperty('--button-position-y', "0rem");
 
         let password = document.getElementById("psw").value;
 
         let passw = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
 
-        if (password.match(passw))
+        if (password.match(/^.{8,32}$/)) {
+            document.getElementById("list-one").style.color = "green";
+        } else {
+            document.getElementById("list-one").style.color = "red";
+        }
+
+        if (password.match(/^(?=.*[a-z])(?=.*[A-Z])/)) {
+            document.getElementById("list-two").style.color = "green";
+        } else {
+            document.getElementById("list-two").style.color = "red";
+        }
+
+        if (password.match(/^(?=.*[0-9])/)) {
+            document.getElementById("list-three").style.color = "green";
+        } else {
+            document.getElementById("list-three").style.color = "red";
+        }
+
+        if (password.match(passw)) {
             resetButtonPosition();
+            document.getElementsByClassName("container")[0].style = "height: 14.5rem";
+            document.documentElement.style.setProperty('--button-position-y', "-5.5rem");
+        }
     } else {
         document.getElementsByClassName("container")[0].style = "height: 12.5rem";
         document.getElementsByClassName("login__button")[0].style = "visibility: hidden";
         resetButtonPosition();
+        document.getElementsByClassName("container")[0].style = "height: 12.5rem";
     }
 }
 
 function resetButtonPosition() {
-    document.documentElement.style.setProperty('--button-position-x', "0");
-    document.documentElement.style.setProperty('--button-position-y', "0");
+    document.documentElement.style.setProperty('--button-position-x', "0rem");
+    document.getElementsByClassName("login__passwordchecklist")[0].style = "visibility: hidden";
 }
 
 function clearInputs() {
@@ -62,6 +86,7 @@ function clearInputs() {
 
     document.getElementsByClassName("container")[0].style = "height: 12.5rem";
     document.getElementsByClassName("login__button")[0].style = "visibility: hidden";
+    document.getElementsByClassName("login__passwordchecklist")[0].style = "visibility: hidden";
 }
 
 function closeModal() {
